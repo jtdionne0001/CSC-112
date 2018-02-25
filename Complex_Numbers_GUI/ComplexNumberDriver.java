b2945a66397bf6bf7b492baebb3042aa938aba0a
Jacob <Jacob@LAPTOP-SBAN8KHO>
2018-02-24 21:13:30 -0500

    Added more operations

--- Complex_Numbers_GUI/ComplexNumberDriver.java
+++ Complex_Numbers_GUI/ComplexNumberDriver.java
@@ -25,24 +25,27 @@ public class ComplexNumberDriver
 		final String title = "Complex Numbers";
 		double real;
 		double imag;
-		
+
 		while (true)
 		{
 			real = getDouble("Enter the Real Component of Complex Number 1", title);
 			imag = getDouble("Enter the Imaginary Component of Complex Number 1", title);
 			Complex c1 = new Complex(real, imag);
-			
+
 			real = getDouble("Enter the Real Component of Complex Number 2", title);
 			imag = getDouble("Enter the Imaginary Component of Complex Number 2", title);
 			Complex c2 = new Complex(real, imag);
 
 			Complex ans = null;
-			
-			String options[] = { "Add", "Subtract", "Multiply", "Divide", "Quit" };
+
+			String options[] =
+			{ "Add", "Subtract", "Multiply", "Divide", "Absolute Value", "Negation", "Conjugate", "Distance",
+					"Comparison", "Quit" };
 			int option = JOptionPane.showOptionDialog(null, "Choose Operation", title, JOptionPane.YES_NO_CANCEL_OPTION,
 					JOptionPane.PLAIN_MESSAGE, null, options, 4);
 			String output = "" + c1;
-			switch (option) {
+			switch (option)
+			{
 			case 0:
 				ans = c1.add(c2);
 				output += " + ";
@@ -59,12 +62,35 @@ public class ComplexNumberDriver
 				ans = c1.divide(c2);
 				output += " / ";
 				break;
+			case 4:
+				output += "=" + c1.abs() + "\n" + c1 + "=" + c2.abs();
+				break;
+			case 5:
+				output += "=" + c1.negate() + "\n" + c1 + "=" + c2.negate();
+				break;
+			case 6:
+				output += "=" + c1.conjugate() + "\n" + c1 + "=" + c2.conjugate();
+				break;
+			case 7:
+				output = "The distance between " + c1 + " and " + c2 + " is " + c1.distance(c2);
+				break;
+			case 8:
+				if (c1.greaterThan(c2))
+					output += " is larger than " + c2;
+				else if (c1.lessThan(c2))
+					output += " is smaller than " + c2;
+				else if (c1.equal(c2))
+					output += " and " + c2 + " are equal or approxamatly equal";
+				else
+					output = "error";
+				break;
 			default:
 				break;
 			}
-			if (option > 3)
+			if (option > 8)
 				break;
-			output += c2 + " = " + ans;
+			else if (option < 4)
+				output += c2 + " = " + ans;
 			JOptionPane.showMessageDialog(null, output, title, JOptionPane.PLAIN_MESSAGE);
 
 			option = JOptionPane.showConfirmDialog(null, "Do you want to do this again?", title,
